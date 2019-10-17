ae0978e810eb6f5af10f2bcc972d5c6d2fea0aa7
ragilpurbawinata <ragilwinataa@gmail.com>
2019-10-17 13:15:50 +0700

    update

--- app/src/main/java/com/rglstudio/coroutines/viewmodel/AlbumViewModel.kt
+++ app/src/main/java/com/rglstudio/coroutines/viewmodel/AlbumViewModel.kt
@@ -3,13 +3,13 @@ package com.rglstudio.coroutines.viewmodel
 import android.content.Context
 import com.rglstudio.coroutines.base.BaseViewModel
 import com.rglstudio.coroutines.network.NetworkConfig
-import com.rglstudio.coroutines.repository.AlbumRepository
+import com.rglstudio.coroutines.repository.AlbumRepositoryImpl
 
 class AlbumViewModel(context: Context) : BaseViewModel(){
     companion object{
         val FACTORY = singleArgViewModelFactory(::AlbumViewModel)
     }
-    private val albumRepository : AlbumRepository = AlbumRepository(NetworkConfig.create(context))
+    private val albumRepository : AlbumRepositoryImpl = AlbumRepositoryImpl(NetworkConfig.create(context))
 
     val responData = albumRepository.responData
     val errorInfo = albumRepository.errorInfo
